aef92137491bcaa90d33f8988f915c5a95d06ed1
Aswathi Unni <aswathi.u@tataelxsi.co.in>
2021-04-03 22:13:10 +0200

    Add files via upload

---
+++ Code Forces/constructString.cpp
@@ -0,0 +1,47 @@
+/*
+   You are given three positive integers n, a and b. You have to construct a string s of length n consisting of lowercase Latin letters such that each substring of length a has exactly b distinct letters. It is guaranteed that the answer exists.
+   You have to answer t independent test cases.
+   Recall that the substring s[l…r] is the string sl,sl+1,…,sr and its length is r−l+1. In this problem you are only interested in substrings of length a.
+   Input
+   The first line of the input contains one integer t (1≤t≤2000) — the number of test cases. Then t test cases follow.
+   The only line of a test case contains three space-separated integers n, a and b (1≤a≤n≤2000,1≤b≤min(26,a)), where n is the length of the required string, a is the length of a substring and b is the required number of distinct letters in each substring of length a.
+   It is guaranteed that the sum of n over all test cases does not exceed 2000 (∑n≤2000).
+   Output
+   For each test case, print the answer — such a string s of length n consisting of lowercase Latin letters that each substring of length a has exactly b distinct letters. If there are multiple valid answers, print any of them. It is guaranteed that the answer exists.
+   Example
+   inputCopy
+   4
+   7 5 3
+   6 1 1
+   6 6 1
+   5 2 2
+   outputCopy
+   tleelte
+   qwerty
+   vvvvvv
+   abcde
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+	int t;
+	cin >> t;
+
+	string ss = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < t; i++) {
+		int n, a, b;
+		cin >> n >> a >> b;
+		string s;
+		for (int j = 0; j < n; j++) 
+			s += ss[j%b];
+		cout << s.c_str() << "\n";
+	}
+
+	return 0;
+}
