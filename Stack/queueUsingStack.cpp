d23ccba77c6dd5990033da7c996810bc24375d43
Aswathi Unni <aswathi.u@tataelxsi.co.in>
2021-07-23 08:07:10 +0200

    Update queueUsingStack.cpp

--- Stack/queueUsingStack.cpp
+++ Stack/queueUsingStack.cpp
@@ -10,28 +10,55 @@ int pop();
 }; */
 
 //Function to push an element in queue by using 2 stacks.
-void StackQueue :: push(int x)
-{
-	s1.push(x);
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyQueue {
+  
+    public:
+        stack<int> stack_newest_on_top, stack_oldest_on_top; 
+        
+        void prepOldStack() {
+            if (stack_oldest_on_top.empty()) {
+                while (!stack_newest_on_top.empty()) {
+                    stack_oldest_on_top.push(stack_newest_on_top.top());
+                    stack_newest_on_top.pop();
+                }
+            }
+        }
+          
+        void push(int x) {
+            stack_newest_on_top.push(x);
+        }
+
+        void pop() {
+            prepOldStack();
+            stack_oldest_on_top.pop();
+        }
+
+        int front() {
+            prepOldStack();
+            return stack_oldest_on_top.top();
+        }
+};
+
+int main() {
+    MyQueue q1;
+    int q, type, x;
+    cin >> q;
+    
+    for(int i = 0; i < q; i++) {
+        cin >> type;
+        if(type == 1) {
+            cin >> x;
+            q1.push(x);
+        }
+        else if(type == 2) {
+            q1.pop();
+        }
+        else cout << q1.front() << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
 }
 
-//Function to pop an element from queue by using 2 stacks.
-int StackQueue :: pop()
-{
-	if (s1.empty())
-		return -1;
-
-	while(!s1.empty()) {
-		s2.push(s1.top());
-		s1.pop();
-	}
-
-	int x = s2.top();
-	s2.pop();
-
-	while(!s2.empty()) {
-		s1.push(s2.top());
-		s2.pop();
-	}
-	return x;
-}
