36b553c04011d1cc217175d4dda2c0425f32cc34
Aswathi Unni <aswaunni@gmail.com>
2021-11-08 08:42:07 +0100

    Create maxIndex.cpp

---
+++ Arrays/maxIndex.cpp
@@ -0,0 +1,46 @@
+/*
+Given an array A[] of N positive integers. The task is to find the maximum of j - i subjected to the constraint of A[i] < A[j] and i < j.
+ 
+
+Example 1:
+
+Input:
+N = 2
+A[] = {1, 10}
+Output:
+1
+Explanation:
+A[0]<A[1] so (j-i) is 1-0 = 1.
+Example 2:
+
+Input:
+N = 9
+A[] = {34, 8, 10, 3, 2, 80, 30, 33, 1}
+Output:
+6
+Explanation:
+In the given array A[1] < A[7]
+satisfying the required 
+condition(A[i] < A[j]) thus giving 
+the maximum difference of j - i 
+which is 6(7-1).
+
+*/
+
+int maxIndexDiff(int a[], int n) 
+{ 
+    multimap<int, int> m;
+    
+    for (int i = 0; i < n; i++)
+        m.insert(pair<int, int>(a[i], i));
+    
+    int x = m.begin()->second;
+    int ans = 0;
+    for (auto& a : m) {
+        if (x <= a.second)
+            ans = max(ans, a.second - x);
+        else
+            x = a.second;
+    }
+    return ans;
+}
