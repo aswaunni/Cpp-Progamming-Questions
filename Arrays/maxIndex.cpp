a2180126ba54cab11f2567b94d8b09eeac0ef482
Aswathi Unni <aswaunni@gmail.com>
2021-11-08 08:48:32 +0100

    Update maxIndex.cpp

--- Arrays/maxIndex.cpp
+++ Arrays/maxIndex.cpp
@@ -44,3 +44,21 @@ int maxIndexDiff(int a[], int n)
     }
     return ans;
 }
+
+//OR
+
+int maxIndexDiff(int a[], int n) 
+{ 
+    int i = 0, j = n-1;
+    int ans = 0;
+    
+    while (i <= j) {
+        if (a[i] <= a[j]) {
+            ans = max(ans, j-i);
+            i++;
+            j = n-1;
+        } else
+            j--;
+    }
+    return ans;
+}
