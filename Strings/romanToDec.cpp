fd843d307e4afa5d9123abc34ed135bd8a1b37f2
Aswathi Unni <aswaunni@gmail.com>
2021-11-09 22:14:09 +0100

    Update romanToDec.cpp

--- Strings/romanToDec.cpp
+++ Strings/romanToDec.cpp
@@ -69,3 +69,23 @@ int main() {
 	return 0;
 }
 
+//OR
+
+int romanToInt(string s) {
+    map<char,int> mp;
+    int n = s.length();
+
+    mp['M']=1000;mp['D']=500;mp['C']=100;mp['L']=50;
+    mp['X']=10;mp['V']=5;mp['I']=1;
+
+    int ans=0;
+
+    for(int i = 0; i < n-1; i++) {
+	    if(mp[s[i]] < mp[s[i+1]])
+		    ans=ans-mp[s[i]];
+	    else 
+		    ans = ans+mp[s[i]];
+    }
+    ans += mp[s[n-1]];
+    return ans;  
+}
