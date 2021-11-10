b3bef2e2eb0ee8977239054e58c2aee417a56e5a
Aswathi Unni <aswaunni@gmail.com>
2021-11-10 21:23:13 +0100

    Update longestPrefix.cpp

--- Strings/longestPrefix.cpp
+++ Strings/longestPrefix.cpp
@@ -1,53 +1,18 @@
 
 //  A C++ Program to find the longest common prefix
-#include<bits/stdc++.h>
-using namespace std;
 
-// A Utility Function to find the common prefix between
-// strings- str1 and str2
-string commonPrefixUtil(string str1, string str2)
-{
-	string result;
-	int n1 = str1.length(), n2 = str2.length();
-
-	// Compare str1 and str2
-	for (int i=0; i < n1 && i < n2; i++)
-	{
-		if (str1[i] != str2[i])
-			break;
-		result.push_back(str1[i]);
-	}
-
-	return (result);
+string longestCommonPrefix(vector<string>& strs) {
+    string s = *min_element(strs.begin(), strs.end());
+    
+    string ans = "";
+    int n = strs.size();
+    
+    for (int i = 0; i < s.length(); i++) {
+        for (int j = 0; j < n; j++) {
+            if (s[i] != strs[j][i])
+                return ans;
+        }
+        ans += s[i];
+    }
+    return ans;
 }
-
-// A Function that returns the longest common prefix
-// from the array of strings
-string commonPrefix (string arr[], int n)
-{
-	string prefix =  arr[0];
-
-	for (int i=1; i<=n-1; i++)
-		prefix = commonPrefixUtil(prefix, arr[i]);
-
-	return (prefix);
-}
-
-// Driver program to test above function
-int main()
-{
-	string arr[] = {"geeksforgeeks", "geeks",
-		"geek", "geezer"};
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	string ans = commonPrefix(arr, n);
-
-	if (ans.length())
-		printf ("The longest common prefix is - %s",
-				ans.c_str());
-	else
-		printf("There is no common prefix");
-
-	return (0);
-}
-
