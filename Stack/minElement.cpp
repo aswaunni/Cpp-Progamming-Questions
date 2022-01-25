cd96d2bfd2797e12cd1b381fe778abe1cf9731ba
Aswathi Unni <aswaunni@gmail.com>
2022-01-25 08:48:31 +0100

    Create minElement.cpp

---
+++ Stack/minElement.cpp
@@ -0,0 +1,67 @@
+/*
+Design a data-structure SpecialStack that supports all the stack operations like push(), pop(), isEmpty(), isFull() and an additional operation getMin() which should return minimum element from the SpecialStack. Your task is to complete all the functions, using stack data-Structure.
+
+
+Example 1:
+
+Input:
+Stack: 18 19 29 15 16
+Output: 15
+Explanation:
+The minimum element of the stack is 15.
+*/
+
+int demoVal = 10001;
+int mn = -1;
+
+void push(stack<int>& s, int a) {
+
+	if (s.empty() || a < mn)
+	    mn = a;
+	    
+	s.push(demoVal*a + mn);
+}
+
+int pop(stack<int>& s){
+	if (s.empty())
+	    return -1;
+	int x = s.top();
+	s.pop();
+	if (s.empty()) 
+	    mn = -1;
+	else
+	    mn = x%demoVal;
+	return x/demoVal;
+}
+
+int getMin(stack<int>& s){
+	return mn;
+}
+
+//OR
+
+stack<int> minS;
+
+void push(stack<int>& s, int a) {
+
+	if (s.empty() || a < minS.top())
+	    minS.push(a);
+	else {
+	    int x = minS.top();
+	    minS.push(x);
+	}
+	s.push(a);
+}
+
+int pop(stack<int>& s){
+	if (s.empty())
+	    return -1;
+	int x = s.top();
+	s.pop();
+	minS.pop();
+	return x;
+}
+
+int getMin(stack<int>& s){
+	return minS.top();
+}
