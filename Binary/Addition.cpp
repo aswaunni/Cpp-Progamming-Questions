a62728b6e752975f00bce67eea0703d57f3d99e7
Aswathi Unni <aswaunni@gmail.com>
2022-02-06 20:36:55 +0100

    Create Addition.cpp

---
+++ Binary/Addition.cpp
@@ -0,0 +1,12 @@
+/*
+Given two integers a and b, return the sum of the two integers without using binary opertaion.
+*/
+
+int getSum(int a, int b) {
+    while (b != 0) {
+        unsigned carry = a&b;
+        a = a^b;
+        b = carry<<1;
+    } 
+    return a;
+}
