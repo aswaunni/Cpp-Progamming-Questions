0723ff800859fc8727c488aed158462893fc8f81
Aswathi Unni <aswaunni@gmail.com>
2022-02-08 21:23:56 +0100

    Create wordDictionary.cpp

---
+++ Dynamic Programming/wordDictionary.cpp
@@ -0,0 +1,44 @@
+/*
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+*/
+
+bool wordBreak(string s, vector<string>& wordDict) {
+    int n = s.length();
+    
+    vector<bool> dp(n+1);
+    dp[n] = true;
+    
+    unordered_set<string> map;
+    for (auto& a : wordDict)
+        map.insert(a);
+    
+    for (int i = n-1; i >= 0; i--) {
+        string sub = "";
+        for (int j = i; j < n; j++) {
+            sub += s[j];
+            if (map.count(sub) && dp[j+1]) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[0];
+}
