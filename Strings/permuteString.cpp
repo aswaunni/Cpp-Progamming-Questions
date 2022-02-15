3fcba969ea8a1f363e119b711f20c01b7268ecd6
Aswathi Unni <aswaunni@gmail.com>
2022-02-15 11:27:29 +0100

    Update permuteString.cpp

--- Strings/permuteString.cpp
+++ Strings/permuteString.cpp
@@ -18,25 +18,39 @@ Given a string S. The task is to print all permutations of a given string.
 
 using namespace std;
 
-void permute(string s, int l, int r)
+void permute(string s, string temp, vector<string>& ans)
 {
-	if (l == r)
-		cout << s << " ";
-	else {
-		for (int i = l; i <= r; i++) {
-			swap(s[l], s[i]);
-
-			permute(s, l+1, r);
-
-			swap(s[l], s[i]);
-		}
-	}
+    if (l == r)
+        ans.push_back(s);
+        
+    for (int i = l; i <= r; i++) {
+        swap(s[i], s[l]);
+        permute(s, l+1, r, ans);
+        swap(s[i], s[l]);
+    }
 }
-int main() {
 
-	string s = "ABC";
-	int n = s.length();
-	permute(s, 0, n-1);
+// OR
 
-	return 0;
+void permute(string s, string temp, vector<string>& ans)
+{
+    if (s.length() == 0) {
+        ans.push_back(temp);
+        return;
+    }
+        
+    for (int i = 0; i < s.length(); i++) {
+        string x = s.substr(0, i) + s.substr(i+1);
+        char c = s[i];
+        permute(x, temp+c, ans);
+    }
 }
+
+vector<string> find_permutation(string s)
+{
+    vector<string> ans;
+    permute(s, "", ans);
+    return ans;
+}
+
+
