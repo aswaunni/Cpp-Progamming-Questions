3a5930895e91e7c1274a58d162b22d58ec7be1f8
Aswathi Unni <aswaunni@gmail.com>
2022-05-20 08:40:35 +0200

    Update sumSubArray.cpp

--- Arrays/sumSubArray.cpp
+++ Arrays/sumSubArray.cpp
@@ -6,25 +6,32 @@ Given an unsorted array A of size N that contains only non-negative integers, fi
 
 using namespace std;
 
+int main()
+{
+    int n;
+    cout << "Enter n :";
+    cin >> n;
 
-int main() {
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+        
+    int sum;
+    cout << "Enter sum : ";
+    cin >> sum;
 
-	int arr[] = {2, 5, 7, 5, 4, 9, 1};
-	int n = sizeof(arr)/sizeof(*arr);
-	int sum = 16;
-
-	for (int i = 0; i < n; i++) {
-		int j = i;
-		int s = 0;
-		while (s < sum && j < n)
-			s += arr[j++];
-
-		if (s == sum) {
-			for (int k = i; k < j; k++)
-				cout << arr[k] << " ";
-			break;
-		}
-	}
-
-	return 0;
+    int s = 0, e = 0, add = 0;
+    for (int i = 0; i < n; i++) {
+        add += arr[i];
+        e = i;
+        while (add > sum && s < i) {
+            add -= arr[s];
+            s++;
+        }
+        if (add == sum) {
+            cout << s+1 << ", " << e+1;
+            break;
+        }
+    }
+    return 0;
 }
