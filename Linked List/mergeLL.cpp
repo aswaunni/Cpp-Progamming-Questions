3af6ee55807df8c76344554cf97bdb0224a6797c
Aswathi Unni <aswaunni@gmail.com>
2022-06-12 16:22:42 +0200

    Update mergeLL.cpp

--- Linked List/mergeLL.cpp
+++ Linked List/mergeLL.cpp
@@ -3,87 +3,101 @@
 using namespace std;
 
 
-struct node {
-	int data;
-	struct node* next;
-	node(int d) 
-	{
-		data = d;
-		next = NULL;
-	}
-	node() 
-	{
-		next = NULL;
-	}
+class Node {
+public:
+    Node(int i) {
+        data = i;
+        next = NULL;    
+    }
+    Node(int i, Node* n) {
+        data = i;
+        next = n;    
+    }
+    Node* next;
+    int data;
 };
 
-struct linkedList {
-	node* head;
-	linkedList()
-	{
-		head = NULL;
-	}
-
-	void push(int d)
-	{
-		node* temp = new node(d);
-		temp->next = head;
-		head = temp;
-	}
-
-	void print()
-	{
-		node* temp = head;
-		while(temp) {
-			cout << temp->data << " ";
-			temp = temp->next;
-		}
-		cout << "\n";
-	}
-
-	void merge(linkedList& l1, linkedList& l2)
-	{
-		node* c1 = l1.head, *c2 = l2.head;
-		node* prev = NULL;
-		while (c1 || c2) {
-			int int1 = (c1 == NULL ? INT_MAX : c1->data);
-			int int2 = (c2 == NULL ? INT_MAX : c2->data);
-			node* r;
-			if (int1 < int2) {
-				r = new node(int1);
-				c1 = (c1 == NULL ? NULL : c1->next);
-			} else {
-				r = new node(int2);
-				c2 = (c2 == NULL ? NULL : c2->next);
-			}
-			if (prev)
-				prev->next = r;
-			if (!head)
-				head = r;
-			prev = r;
-		}
-	}
-};
-
-int main() {
-
-	linkedList l1, l2;
-	l1.push(19);
-	l1.push(15);
-	l1.push(8);
-	l1.push(4);
-	l1.print();
-	l2.push(16);
-	l2.push(10);
-	l2.push(9);
-	l2.push(7);
-	l2.print();
-
-	linkedList l3;
-	l3.merge(l1, l2);
-	l3.print();
-
-	return 0;
+void insertLL(Node* &head, int i) {
+    Node* temp = head;
+    Node* n = new Node(i);
+    
+    if(head == NULL)
+        head = n;
+    while(temp->next)
+        temp = temp->next;
+    temp->next = n;
 }
 
+void printLL(Node* head) {
+    Node* temp = head;
+    while(temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
 
+Node* mergeLL(Node* h1, Node* h2){
+    Node* dummy = new Node(-1);
+    Node* newHead = dummy;
+    while(h1 && h2) {
+        if (h1->data < h2->data) {
+            dummy->next = h1;
+            h1 = h1->next;
+            dummy = dummy->next;
+        } else {
+            dummy->next = h2;
+            h2 = h2->next;
+            dummy = dummy->next;
+        }
+    }
+
+    while(h1) {
+        dummy->next = h1;
+        h1 = h1->next;
+        dummy = dummy->next;
+    }
+
+    while (h2) {
+        dummy->next = h2;
+        h2 = h2->next;
+        dummy = dummy->next;
+    }
+    return newHead->next;
+}
+
+Node* mergeLLRecursive(Node* h1, Node* h2) {
+    if (h1 == NULL) 
+        return h2;
+
+    if (h2 == NULL)
+        return h1;
+    
+    Node* result;
+    if (h1->data < h2->data) {
+        result = h1;
+        result->next = mergeLLRecursive(h1->next, h2);
+    } else {
+        result = h2;
+        result->next = mergeLLRecursive(h1, h2->next);
+    }
+    return result;
+}
+
+int main()
+{
+    Node* l1 = new Node(1);
+    insertLL(l1, 4);
+    insertLL(l1, 5);
+    insertLL(l1, 7);
+    Node* l2 = new Node(2);
+    insertLL(l2, 3);
+    insertLL(l2, 6);
+
+    printLL(l1);
+    printLL(l2);
+
+    printLL(mergeLLRecursive(l1, l2));
+
+    return 0;
+}
