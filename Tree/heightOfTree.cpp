654aa65f7b7768fabe9f7f4c560ee57048e0d044
Aswathi Unni <aswaunni@gmail.com>
2022-06-27 09:27:45 +0200

    Create heightOfTree.cpp

---
+++ Tree/heightOfTree.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    Node* left;
+    Node* right;
+    int data;
+    
+    Node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int height(Node* r) {
+    if (r == NULL)
+        return 0;
+        
+    return 1+ max(height(r->left), height(r->right));
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    
+    cout << height(root);
+}
