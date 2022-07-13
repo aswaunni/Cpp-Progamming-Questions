783ec5fb7e6cb745bbe4b419ddb8a1bdf3dc69c5
Aswathi Unni <aswaunni@gmail.com>
2022-07-13 09:04:48 +0200

    Create partitionEqualSubsetSum.cpp

---
+++ Arrays/partitionEqualSubsetSum.cpp
@@ -0,0 +1,52 @@
+/*
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+ */
+
+// DP
+
+bool canPartition(vector<int>& nums) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    
+    if (sum%2 == 1)
+        return false;
+    
+    vector<bool> dp(sum/2, false);
+    dp[0] = true;
+    
+    for (int i = 1; i <= nums.size(); i++) {
+        for (int j = sum/2; j > 0; j--) {
+            if (j >= nums[i-1])
+                dp[j] = dp[j] | dp[j - nums[i-1]];
+        }
+    }
+    return dp[sum/2];
+}
+
+// Bit Manipulation
+
+bool canPartition(vector<int>& nums) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    
+    if (sum%2 == 1)
+        return false;
+    
+    bitset<10001> bits(0);
+    bits[0] = 1;
+    
+    for (auto& a : nums)
+        bits |= (bits << a);
+
+    return bits[sum >> 1];
+}
