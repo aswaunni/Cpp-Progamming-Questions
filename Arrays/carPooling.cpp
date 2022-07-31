beac8bdf14667fdb5ff1ae417bc32b7a8a872591
Aswathi Unni <aswaunni@gmail.com>
2022-07-31 12:23:37 +0200

    Create carPooling.cpp

---
+++ Arrays/carPooling.cpp
@@ -0,0 +1,34 @@
+/*
+There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+
+You are given the integer capacity and an array trips where trips[i] = [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are given as the number of kilometers due east from the car's initial location.
+
+Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+Output: false
+Example 2:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+Output: true
+ */
+
+bool carPooling(vector<vector<int>>& trips, int capacity) {
+    vector<int> v(1001, 0);
+    
+    for (auto& a : trips) {
+        v[a[1]] += a[0];
+        v[a[2]] -= a[0];
+    }
+    
+    for (auto& a : v) {
+        capacity -= a;
+        if (capacity < 0)
+            return false;
+    }
+    return true;
+}
