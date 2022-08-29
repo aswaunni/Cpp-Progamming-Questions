eab65c4d6be6245cfe07732d55fd2a3476992e04
Aswathi Unni <aswaunni@gmail.com>
2022-08-29 09:19:29 +0200

    Create kClosest.cpp

---
+++ Arrays/kClosest.cpp
@@ -0,0 +1,48 @@
+/*
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]
+*/
+
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int left = 0, right = arr.size()-k; // left and right of start of the range of size k
+    
+    while (left < right) {
+        int mid = (left + right)/2;
+        
+        if (x - arr[mid] > arr[mid+k] - x)
+            left = mid+1;
+        else
+            right = mid;
+    }
+    
+    return vector<int>(arr.begin() + left, arr.begin() + left + k);
+}
+
+// OR (O(n))
+
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int left = 0, right = arr.size()-1;
+    
+    while ((right - left) >= k) {
+        if (x - arr[left] > arr[right] - x)
+            left++;
+        else
+            right--;
+    }
+    
+    return vector<int>(arr.begin() + left, arr.begin() + left + k);
+}
