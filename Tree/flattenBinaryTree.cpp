24dceb36981567146d06f74bc627d841422fb1fb
Aswathi Unni <aswaunni@gmail.com>
2022-08-31 17:28:28 +0200

    Update flattenBinaryTree.cpp

--- Tree/flattenBinaryTree.cpp
+++ Tree/flattenBinaryTree.cpp
@@ -1,74 +1,51 @@
 /*
-Given a binary tree, flatten it into a linked list in place. After flattening, left of each node should point to NULL
-and right should containext nod in preorder sequence.
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+Example 1:
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
 */
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-struct Node {
-    Node* left;
-    Node* right;
-    int data;
     
-    Node(int d) {
-        data = d;
-        left = NULL;
-        right = NULL;
+void flatten(TreeNode* root) {
+    while (root) {
+        if (root->left) {
+            TreeNode* t = root->left;
+            while(t->right)
+                t = t->right;
+            
+            t->right = root->right;
+            root->right = root->left;
+            root->left = NULL;
+        }
+        root = root->right;
     }
-};
+}	
 
-Node* flatten(Node* r) {
-    if (r == NULL)
-        return NULL;
-    
-    Node* lHead = flatten(r->left);
-    Node* rHead = flatten(r->right);
-    
-    r->left = NULL;
-    r->right = lHead;
-    
-    Node* t = r;
-    while(t->right != NULL)
-        t = t->right;
-        
-    t->right = rHead;
-    
-    return r;
-}
+// OR (Recursion)
 
-// OR 
-
-Node* flatten(Node* r, Node* prev) {
-    if (r == NULL)
-        return prev;
-        
-    prev = flatten(r->right, prev);
-    prev = flatten(r->left, prev);
+TreeNode* prev = NULL;
+void flatten(TreeNode* root) {
+    if (!root)
+        return;
     
-    r->right = prev;
-    r->left = NULL;
-    prev = r;
-    return prev;
-}
-
-int main()
-{
-    Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
+    flatten(root->right);
+    flatten(root->left);
     
-    Node* prev = NULL;
-    Node* r = flatten(root, prev);
-    
-    while(r) {
-        cout << r->data << " ";
-        r = r->right;
-    }
-    return 0;
+    root->right = prev;
+    root->left = NULL;
+    prev = root;
 }
