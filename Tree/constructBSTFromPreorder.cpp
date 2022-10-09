e432e20de38d8dd8d44a4cfc377d4808c8332f1a
Aswathi Unni <aswaunni@gmail.com>
2022-10-09 11:54:25 +0200

    Update constructBSTFromPreorder.cpp

--- Tree/constructBSTFromPreorder.cpp
+++ Tree/constructBSTFromPreorder.cpp
@@ -1,48 +1,46 @@
-#include <bits/stdc++.h>
+/*
+Given an array of integers preorder, which represents the preorder traversal of a BST (i.e., binary search tree), construct the tree and return its root.
 
-using namespace std;
+It is guaranteed that there is always possible to find a binary search tree with the given requirements for the given test cases.
 
-struct Node {
-    Node* left;
-    Node* right;
-    int data;
-    
-    Node(int d) {
-        data = d;
-        left = NULL;
-        right = NULL;
-    }
-};
+A binary search tree is a binary tree where for every node, any descendant of Node.left has a value strictly less than Node.val, and any descendant of Node.right has a value strictly greater than Node.val.
 
-Node* constructBSTFromPreorder(int pre[], int& preIdx, int x, int mn, int mx, int n) {
-    if (preIdx >= n)
+A preorder traversal of a binary tree displays the value of the node first, then traverses Node.left, then traverses Node.right.
+*/
+
+int i = 0;
+TreeNode* bstFromPreorder(vector<int>& preorder, int mx = INT_MAX) {
+    if (i == preorder.size() || preorder[i] >= mx)
         return NULL;
-        
-    Node* r = NULL;
-    if (x > mn && x < mx) {
-        r = new Node(x);
-        preIdx++;
-        if (preIdx < n) {
-            r->left = constructBSTFromPreorder(pre, preIdx, pre[preIdx], mn, x, n);
-            r->right = constructBSTFromPreorder(pre, preIdx, pre[preIdx], x, mx, n);
+
+    TreeNode* t = new TreeNode(preorder[i++]);
+
+    t->left = bstFromPreorder(preorder, t->val);
+    t->right = bstFromPreorder(preorder, mx);
+
+    return t;
+}
+
+// OR
+
+TreeNode* bstFromPreorder(vector<int>& preorder) {
+    stack<TreeNode*> s;
+    TreeNode* root = new TreeNode(preorder[0]);
+    s.push(root);
+
+    for (int i = 1; i < preorder.size(); i++) {
+        TreeNode* n = new TreeNode(preorder[i]);
+        if (s.top()->val > preorder[i])
+            s.top()->left = n;
+        else {
+            TreeNode* t = NULL;
+            while (!s.empty() && s.top()->val < preorder[i]) {
+                t = s.top();
+                s.pop();
+            }
+            t->right = n;
         }
+        s.push(n);
     }
-    return r;
-}
-
-void preOrder(Node* r) {
-    if (r == NULL)
-        return;
-    cout << r->data << " ";
-    preOrder(r->left);
-    preOrder(r->right);
-}
-
-int main()
-{
-    int pre[] = {10, 2, 1, 13, 11};
-    int preIdx = 0;
-    Node* root = constructBSTFromPreorder(pre, preIdx, pre[0], INT_MIN, INT_MAX, 5);
-    preOrder(root);
-    return 0;
+    return root;
 }
