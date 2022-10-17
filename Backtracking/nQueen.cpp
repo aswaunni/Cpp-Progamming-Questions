a365f98ce5117ec4341f34112863faa53d4084df
Aswathi Unni <aswaunni@gmail.com>
2022-10-17 13:01:03 +0200

    Update nQueen.cpp

--- Backtracking/nQueen.cpp
+++ Backtracking/nQueen.cpp
@@ -1,62 +1,46 @@
-/*
-Place n queens in an n*n matrix
+/*The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+ 
+
+Example 1:
+
+
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+Example 2:
+
+Input: n = 1
+Output: [["Q"]]
 */
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
-bool isSafe(int** arr, int x, int y, int n)
-{
-    for (int i = 0; i < x; i++)
-        if (arr[i][y])
-            return false;
-
-    int i = x, j = y;
-    while (i >= 0 && j >= 0)
-        if (arr[i--][j--])
-            return false;
-    
-    i = x, j = y;
-    while (i >= 0 && j < n)
-        if (arr[i--][j++])
-            return false;
-    return true;
-}
-
-bool nQueen(int** arr, int x, int n) {
-    if (x >= n)
-        return true;
-
-    for (int j = 0; j < n; j++) {
-        if (isSafe(arr, x, j, n)){
-            arr[x][j] = 1;
-            if (nQueen(arr, x+1, n))
-                return true;
-            arr[x][j] = 0;
-        }
+void helper(int row, int n, vector<vector<string>>& ans, vector<string>& mtx, vector<bool>& col, vector<bool>& diag1, vector<bool>& diag2) {
+    if (row == n) {
+        ans.push_back(mtx);
+        return;
     }
-    return false;
-}
-   
-int main()
-{
-    int n;
-    cout << "Enter n: ";
-    cin >> n;
 
-    int** arr = new int*[n];
     for (int i = 0; i < n; i++) {
-        arr[i] = new int[n];
-        for (int j = 0; j < n; j++)
-            arr[i][j] = 0;
-    }
-
-    if (nQueen(arr, 0, n)) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++)
-                cout << arr[i][j] << " ";
-            cout << endl;
+        if (col[i] && diag1[row+i] && diag2[n-1+i-row]) {
+            col[i] = diag1[row+i] = diag2[n-1+i-row] = false;
+            mtx[row][i] = 'Q';
+            helper(row+1, n, ans, mtx, col, diag1, diag2);
+            col[i] = diag1[row+i] = diag2[n-1+i-row] = true;
+            mtx[row][i] = '.';
         }
     }
+}
+vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> ans;
+    vector<string> mtx(n, string(n, '.'));
+    vector<bool> col(n, true);
+    vector<bool> diag1(2*n-1, true);
+    vector<bool> diag2(2*n-1, true);
+
+    helper(0, n, ans, mtx, col, diag1, diag2);
+    return ans;
 }
